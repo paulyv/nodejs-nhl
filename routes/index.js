0d5122a49fc9fa3e5f725879557efaa8291e83b8
paulyv <pauli.varelius@gmail.com>
2015-12-06 03:15:01 +0200

    removed some unnecessary comments

--- routes/index.js
+++ routes/index.js
@@ -18,10 +18,11 @@ router.get('/scores', function(req, res, next) {
   });
 });
 
+// Returns the stream links..
 router.get('/gameurl/:year/:season/:game', function(req, res, next) {
     request('http://smb.cdnak.neulion.com/fs/nhl/mobile/feed_new/data/streams/'+req.params.year+'/ipad/'+req.params.season+'_'+req.params.game+'.json', function (error, response, body) {
         if (!error && response.statusCode == 200) {
-            console.log(body) // Show the HTML for the Google homepage.
+            console.log(body);
             var json = JSON.parse(body);
             res.json(json);
         }
